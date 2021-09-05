fd505738162fc3c5228f4202429e1701cd1285b8
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2021-09-05 16:50:52 +0300

    Remove replies from email body

--- src/EmailBodyParser.php
+++ src/EmailBodyParser.php
@@ -6,6 +6,12 @@ class EmailBodyParser
 {
     public function parse(string $data, string $delimiter) : string
     {
+        $pos = strpos($data, $delimiter);
+
+        if (false !== $pos) {
+            $data = substr($data, 0, $pos);
+        }
+
         return $data;
     }
 }
