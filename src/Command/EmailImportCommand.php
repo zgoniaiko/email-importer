adb45e27eea5a5327d31a1670f14816275bfa7e3
Ivan Zgoniaiko <zgoniaiko@gmail.com>
2021-09-05 16:58:45 +0300

    Switch to symfony command

--- src/Command/EmailImportCommand.php
+++ src/Command/EmailImportCommand.php
@@ -7,8 +7,7 @@ use App\EmailTransformerCollection;
 use App\Entity\Email;
 use App\Entity\EmailProvider;
 use App\Repository\EmailProviderRepository;
-use Doctrine\ORM\Tools\Console\Command\AbstractEntityManagerCommand;
-use Doctrine\ORM\Tools\Console\EntityManagerProvider;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -16,29 +15,30 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class EmailImportCommand extends AbstractEntityManagerCommand
+class EmailImportCommand extends Command
 {
     protected static $defaultName = 'app:email:import';
     protected static $defaultDescription = 'Import emails';
+    private EntityManagerInterface $em;
     private EmailAdapterCollection $adapters;
     private EmailTransformerCollection $transformers;
 
     public function __construct(
         EmailAdapterCollection $adapters,
         EmailTransformerCollection $transformers,
-        ?EntityManagerProvider $entityManagerProvider = null
+        EntityManagerInterface $em
     ) {
         $this->adapters = $adapters;
         $this->transformers = $transformers;
+        $this->em = $em;
 
-        parent::__construct($entityManagerProvider);
+        parent::__construct();
     }
 
     protected function configure(): void
     {
         $this
             ->addOption('batch-size', null, InputArgument::OPTIONAL, 'Maximum amount of items in RAM', 500)
-            ->addOption('em', null, InputOption::VALUE_REQUIRED, 'Name of the entity manager to operate on')
         ;
     }
 
@@ -47,10 +47,8 @@ class EmailImportCommand extends AbstractEntityManagerCommand
         $io = new SymfonyStyle($input, $output);
         $batchSize = $input->getOption('batch-size');
 
-        $em = $this->getEntityManager($input);
-
         /** @var EmailProviderRepository $providerRepo */
-        $providerRepo = $em->getRepository(EmailProvider::class);
+        $providerRepo = $this->em->getRepository(EmailProvider::class);
         $emailProviders = $providerRepo->getProviderQuery(new \DateTime());
 
         $itemsAdded = 0;
@@ -65,21 +63,21 @@ class EmailImportCommand extends AbstractEntityManagerCommand
                 $email = $this->transformers->fromArray($platform, $data);
                 $email->setEmailProvider($provider);
 
-                $em->persist($email);
+                $this->em->persist($email);
 
                 if (0 === ($itemsAdded % $batchSize)) {
-                    $em->flush();
-                    $em->clear();
+                    $this->em->flush();
+                    $this->em->clear();
                 }
 
                 $itemsAdded++;
             }
 
             $provider->setExecutedAt(new \DateTime());
-            $em->persist($provider);
+            $this->em->persist($provider);
 
-            $em->flush();
-            $em->clear();
+            $this->em->flush();
+            $this->em->clear();
         }
 
         $io->info('DONE!');
